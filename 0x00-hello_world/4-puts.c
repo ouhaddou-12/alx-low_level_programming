0cdd358bacd1229b19358bc6b8b98d5a6d065f7f
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-16 16:47:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functio
+ n
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzz
+le");
+return (0);
+}
+
