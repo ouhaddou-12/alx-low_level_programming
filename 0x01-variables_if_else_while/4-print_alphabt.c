ae58e0f992b56f1ae044cdb94df0f361113df14d
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 14:45:26 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
