3524954864ddf54f29bb7d356eeb9838d308594d
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 15:18:08 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
