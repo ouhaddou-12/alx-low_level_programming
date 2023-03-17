d36bdc31c388a28b43a3b760f2970f8bdcd4da42
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-16 17:01:16 -0700

    4-putsr.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
-/**
- * main - A program that prints a line with puts functio
- n
- * Return: 0 (success)
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzz
-le");
-return (0);
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+        retunr (0);
 }
