a9aa733f2e6c5a7bd9fab887c69c35cd8f427f71
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 14:58:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
