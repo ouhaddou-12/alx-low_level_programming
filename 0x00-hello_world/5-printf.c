453523039661651f88d63b4e262283d56a351114
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-16 17:09:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Entry point
+ *
+ * discription; A C program that print with printf function.
+ *
+ * Return:*/
+
+int main(void){
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
