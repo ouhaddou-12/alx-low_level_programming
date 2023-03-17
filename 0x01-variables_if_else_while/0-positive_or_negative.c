7b478afa28b927d3bc9188aa2f915cd74b31fe5b
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 10:44:21 -0700

    create new files

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+int main(void)
+{
+int n;
+if (n > 0)
+	printf("%d is positive", n);
+else if (n < 0)
+	printf("%d is negative", n);
+else 
+	printf("%d is zero", n);	
+return(0);
