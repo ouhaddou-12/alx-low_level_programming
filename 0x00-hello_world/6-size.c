e9c6e27208449e843aa391e1f4e8aff33d8dc49e
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 08:06:18 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+int main(void)
+{
+printf("size of char: %d", sizeof(char));
+printf("size of int: %d", sizeof(int));
+printf("size of long int: %d", sizeof(long int));
+printf("size of long long int: %d", sizeof(long long int));
+printf("size of float: %d", sizeof(float));
+return (0);
+}
