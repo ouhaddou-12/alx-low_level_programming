759e61128800628cfade8098efe2fab31d42f4d5
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 08:47:24 -0700

    ayoub

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-printf("size of char: %lu bayt(s)\n", sizeof(char));
-printf("size of int: %lu bayt(s)\n", sizeof(int));
-printf("size of long int: %lu bayt(s)\n", sizeof(long int));
-printf("size of long long int: %lu bayt(s)\n", sizeof(long long int));
-printf("size of float: %lu bayt(s)\n", sizeof(float));
+printf("size of char: %u bayt(s)\n", sizeof(char));
+printf("size of int: %u bayt(s)\n", sizeof(int));
+printf("size of long int: %u bayt(s)\n", sizeof(long int));
+printf("size of long long int: %u bayt(s)\n", sizeof(long long int));
+printf("size of float: %u bayt(s)\n", sizeof(float));
 return (0);
 }
