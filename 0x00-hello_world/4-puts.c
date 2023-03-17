35ef3c27b7b3a573389c3ee7d6e8d910ed2b2b2b
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-17 07:47:37 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-        retunr (0);
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
