f45efa12fbb474646426a8cf9efdaf75641ee90b
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-21 16:33:36 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * print_last_digit - absolute value
+ *
+ * @n: takes integer type input for function
+ *
+ * Return: return last digit
+*/
+
+void jack_bauer(void)
+{
+int j;
+for (j = 0; j < 1440; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
