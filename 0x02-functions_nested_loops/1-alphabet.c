d36410a9df404dee5bc1a14146133bdb02aff81c
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-21 14:04:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
