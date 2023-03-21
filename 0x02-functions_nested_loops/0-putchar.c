89f2adee226450ae164bbd79653bf23f7bfb3f81
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-21 10:33:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: c program running
+ *
+ * Return: Always return 0
+*/
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
