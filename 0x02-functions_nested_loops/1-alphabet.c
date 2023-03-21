12bb98b8cc873fedd718515f1ad165996aee009a
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-21 15:25:23 -0700

    1-alphabetf.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,15 @@
 #include "main.h"
-/* more headers goes there */
+
 /**
- * main - Entry point
- *
- * Description: c program running
- *
- * Return: Always return 0
+ * print_alphabet - utilizes on the _putchar function to print
 */
 
-/* betty style doc for function main goes there */
 void print_alphabet(void)
 {
-	int n = 97;
-
-	while (n <= 122)
-	{
-		putchar(n);
-		n++;
-	}
-	putchar('\n');
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
