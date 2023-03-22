aa3cdc5e0c7008086cf7d97e2eecc897d6783f1c
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-22 15:40:47 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print fibbonachi squence 4 million
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+float total_sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
+while (1)
+{
+sum = fib1 + fib2;
+if (sum > 4000000)
+{
+break;
+}
+if ((sum % 2) == 0)
+{
+total_sum += sum;
+}
+fib1 = fib2;
+fib2 = sum;
+}
+printf("%.0f\n", total_sum);
+return (0);
+}
