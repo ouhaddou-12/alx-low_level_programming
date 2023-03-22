33a85bd294b29b4fbae146dc0a429ef439e7ebd7
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-22 07:53:59 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add adition number,
+ *
+ * @a first num
+ * @b second num
+ *
+ * return (0);
+*/
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
