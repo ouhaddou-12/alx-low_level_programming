c2a4f1cadf685aade02fea176176eede2e791650
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-22 15:24:30 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *  * main - divide on 3 and 5
+ *   *
+ *    * description: odd or even
+ *     *
+ *      * Return: always 0 (Success)
+ *      */
+
+int main(void)
+{
+int sum, num;
+for (num = 0; num <= 1024; num++)
+{
+if ((num % 3) == 0 || (num % 5) == 0)
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}
