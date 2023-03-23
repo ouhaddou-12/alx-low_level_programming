8fc28c4462b455d7aab20f930a57f304a3a724f5
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-23 10:28:08 -0700

    f

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+#include <ctype.h>
+
+/**
+ *  * print_numbers - order numbers
+ *   * description: print numbers ascd
+ *   */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+					{
+								_putchar(i);
+									}
+		_putchar('\n');
+}
