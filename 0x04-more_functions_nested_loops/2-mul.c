85b06c107e35da3e1a735d918340e6170793af3a
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-23 10:07:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - mul numbers
+ * description: mutli two number
+ * @a: first parameter
+ * @b: second parameter
+ * Return: return 1 if upper otherwise 0
+*/
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
