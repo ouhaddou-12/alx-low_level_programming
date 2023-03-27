df7b59bc171bfee823c794ce9c5b447c93dd7bf5
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-27 12:59:35 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  * _puts - prints a string
+ *   * @str: the string
+ *    *
+ *     * Return: the length of the string
+ *      */
+void _puts(char *str)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
