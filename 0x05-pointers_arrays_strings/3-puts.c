1d0c7517e34d7a51ce73d04f7a8b6ccf9a201ca2
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-27 15:19:43 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts - function that print str
- * description: string printing
- * @str: string to print
- * Return: always 0
-*/
+ *  * _puts - function that print str
+ *   * description: string printing
+ *    * @str: string to print
+ *     * Return: always 0
+ *     */
 
 void _puts(char *str)
 {
