01a9f5b20d79c997f7d6036b22a98a4c2a0723d1
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-27 14:57:34 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- *  * _puts - prints a string
- *   * @str: the string
- *    *
- *     * Return: the length of the string
- *      */
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+*/
 void _puts(char *str)
 {
-	char *str;
+	int i = 0;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+												}
+	_putchar('\n');
 }
