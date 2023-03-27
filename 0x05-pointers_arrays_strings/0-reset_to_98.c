360dee8c6fc7d6f487b067129fce12fcc9b24ff8
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-27 10:23:29 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that reset number
+ * description: number 98 greather or less reset to it
+ * @n: input
+ * update value n to 98
+ * Return: n
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
