9c9fb6f591f3d92cf09cc151bf1230f339016a22
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-03-27 15:13:03 -0700

    3-puts.cv5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: the string
- *
- * Return: the length of the string
+ * _puts - function that print str
+ * description: string printing
+ * @str: string to print
+ * Return: always 0
 */
+
 void _puts(char *str)
 {
-
 	while (*str != '\0')
 	{
 		_putchar(*str++);
