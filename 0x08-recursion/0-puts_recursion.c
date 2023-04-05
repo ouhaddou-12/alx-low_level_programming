c283dabe97388fd37d0831614c57e3ccee4835aa
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-04-05 07:54:49 -0700

    0-puts_recursion.c v1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print string
- * @*s - input string
- * return void
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
 */
 
 void _puts_recursion(char *s);
