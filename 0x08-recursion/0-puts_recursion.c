75573f742b50e4c7b8e71693471d74e558b0e0c7
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-04-05 07:56:31 -0700

    0-puts_recursion.c v2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,16 @@
  * Return:void
 */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
