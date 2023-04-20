bd1f1952779dbc835711de758b96e48e414a1446
Ayoub OUHADDOU <ouhaddou7398@gmail.com>
2023-04-20 03:22:30 -0700

    z

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 #include "main.h"
-
+#include <srdlib.h>
 /**
  * array_iterator - executes a function given as a
  * parameter on each element of an array.
@@ -12,9 +12,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	if (array && size && action)
+	{
+		int *len = array + size - 1;
 
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+		while (array <= len)
+			action(*array++);
+	}
+
 }
